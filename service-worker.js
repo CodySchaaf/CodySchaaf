03ed4882d1a0a47e40de7cbed15d47f3953876a5
CodySchaaf <schaaf@sigfig.com>
2020-08-15 20:57:50 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,38 +1,13 @@
-var cacheName = "sgtoilet-cache-" + Date.now();
-var filesToCache = [
-  "/",
-  "/index.html",
-  "/global.css",
-  "/build/bundle.css",
-  "/build/bundle.js",
-  "/fonts/open-sans-latin-700.woff2",
-  "/fonts/open-sans-latin-400.woff2"
-];
-self.addEventListener("install", function(e) {
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(filesToCache);
-    })
-  );
+self.addEventListener('install', function(e) {
+  self.skipWaiting();
 });
-self.addEventListener("activate", e => {
-  e.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(thisCacheName) {
-          if (thisCacheName !== cacheName) {
-            return caches.delete(thisCacheName);
-          }
-        })
-      );
+
+self.addEventListener('activate', function(e) {
+  self.registration.unregister()
+    .then(function() {
+      return self.clients.matchAll();
     })
-  );
-});
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    (async function() {
-      const response = await caches.match(e.request);
-      return response || fetch(e.request);
-    })()
-  );
+    .then(function(clients) {
+      clients.forEach(client => client.navigate(client.url))
+    });
 });
